11cc415ffac336c6d22f25b49fff7edcb82f444f
mandularahul <71830393+mandularahul@users.noreply.github.com>
2021-01-22 20:35:50 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,15 @@
+//import firebase from 'firebase'
+//require ("firebase/firestore")
+
+
+  // Your web app's Firebase configuration
+  ///var firebaseConfig = {
+   // apiKey: "AIzaSyAOa68ETNbWmHazarIq0C8caASWFwgAILg",
+    //authDomain: "story-hub-731d7.firebaseapp.com",
+    //projectId: "story-hub-731d7",
+   // storageBucket: "story-hub-731d7.appspot.com",
+    ////messagingSenderId: "981826723806",
+    //appId: "1:981826723806:web:016dd0841a5b088426c74f"
+  //};
+  // Initialize Firebase
+  //firebase.initializeApp(firebaseConfig);
